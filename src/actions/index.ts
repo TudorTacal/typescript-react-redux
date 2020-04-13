43c35a2968475787098d690f22e2dfc3723fe97c
TudorTacal <tudortacal@gmail.com>
2020-04-13 15:36:40 +0100

    feat: render a list

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -13,7 +13,7 @@ export interface FetchTodosAction {
   payload: Todo[];
 }
 
-const url = 'https://jasonplaceholder.typicode.com/todos';
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
