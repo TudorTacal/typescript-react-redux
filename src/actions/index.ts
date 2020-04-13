a9f6e8a350275a854ed2143fe8c70a191d30bf3b
TudorTacal <tudortacal@gmail.com>
2020-04-13 15:24:53 +0100

    feat: set up redux - crete action, reducer, types

---
+++ src/actions/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: true;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+const url = 'https://jasonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    })
+  };
+}
