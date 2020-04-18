ba53075d61e1618403cc2ab028856b2296e240f5
TudorTacal <tudortacal@gmail.com>
2020-04-18 22:00:40 +0100

    feat: create delete todo action creator

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -13,15 +13,29 @@ export interface FetchTodosAction {
   payload: Todo[];
 }
 
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
+    // the return type of response data is Todo[]
     const response = await axios.get<Todo[]>(url);
 
+    // the argument that is expected to be passed to dispatch
     dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data,
-    })
+    });
   };
-}
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id,
+  };
+};
